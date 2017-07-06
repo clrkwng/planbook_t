53478edc9cf3b1261c6d75b0ed11049daa99b19b
Andrew Parise <andrew.parise@spartasystems.com>
2017-07-06 18:33:00 -0400

    Andrew Parise:  - Project structure fix (WIP)  - .gitignore added
     Note: Changes untested due to: dependency on live data source & lack of unit test implementation

--- checklist/template_items/meta_tags.php
+++
@@ -1,7 +0,0 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<meta name="description" content="Simple and Clean Checklist">
-<meta name="author" content="Kevin Yan">
-<!-- <link rel="icon" href="../../favicon.ico"> -->
