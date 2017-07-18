dec27016902196b2809f5115fe7d51c0f00750b2
Andrew Parise <andrew.parise@spartasystems.com>
2017-07-17 21:08:30 -0400

    Andrew Parise:  - Build Automation tasks (WIP)  - Dependency management integration (WIP)

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    //Grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+    //Build Task Registration
+    grunt.registerTask('default',
+        ['uglify']
+    );
+
+
+};
