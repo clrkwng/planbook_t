823325e643ebfcd54bf957022dceb38436dc7fc2
clwang2 <clwang07733@gmail.com>
2017-08-05 19:29:46 -0400

    Clark Wang/Kevin Yan:
    -Login.php now initiates the current date

--- src/AppBundle/views/auth/Login.php
+++ src/AppBundle/views/auth/Login.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-$username = $password = '';
-
+ini_set('display_errors',0);
 if (isset($_COOKIE['username']) and isset($_COOKIE['password'])){
     $cookieUsername = $_COOKIE['username'];
     $cookiePassword = $_COOKIE['password'];
@@ -9,73 +8,53 @@ if (isset($_COOKIE['username']) and isset($_COOKIE['password'])){
 
 
 require_once "../../scripts/dbcomm.php";
-require_once "../../scripts/Crypto.php";
-
 //create db connection
 $dbcomm = new dbcomm();
 
-if (isset($_POST['submitCredentials'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    if($dbcomm->verifyCredentials($username, sha1($password))) {
+if (isset($_POST['Submit'])) {
+    if(isset($_POST['signin-username']) and isset($_POST['signin-password'])) {
+        if($dbcomm->verifyCredentials($_POST['signin-username'], sha1($_POST['signin-password']))) {
 
-        if($dbcomm->isAccountVerified($username)) {
-           if(isset($_POST['remember'])){
-               if($_POST['remember']=='yes'){
-                   $cookie_name_username = 'username';
-                   $cookie_value_username = $username;
-                   setcookie($cookie_name_username, $cookie_value_username, time() + 60*60*24*7);
-                   $cookie_name_password = 'password';
-                   $cookie_value_password = $password;
-                   setcookie($cookie_name_password, $cookie_value_password, time() + 60*60*24*7);
-               }
-           }
-            $type = $dbcomm->getTypeByUsername($_POST['username']);
-            if($type == "Admin") {
-                $encryptedUsername = Crypto::encrypt($username, true);
-                echo "<script>window.location = '../admin/AdminPanel.php?id=$encryptedUsername'</script>";
+            $username = $_POST['signin-username'];
+
+            if($dbcomm->isAccountVerified($_POST['signin-username'])) {
+                if($_POST['remember']=='yes'){
+                    $cookie_name_username = 'username';
+                    $cookie_value_username = $_POST['signin-username'];
+                    setcookie($cookie_name_username, $cookie_value_username, time() + 60*60*24*7);
+                    $cookie_name_password = 'password';
+                    $cookie_value_password = $_POST['signin-password'];
+                    setcookie($cookie_name_password, $cookie_value_password, time() + 60*60*24*7);
+                }
+                $type = $dbcomm->getTypeByUsername($_POST['signin-username']);
+                if($type == "Admin") {
+                    $encryptedUsername = openssl_encrypt($username,'bf-cfb','adminPanelPassword');
+                    $encryptedUsername = str_replace("+", "!!!", $encryptedUsername);
+                    $encryptedUsername = str_replace("%", "$$$", $encryptedUsername);
+                    echo "<script>window.location = '../admin/AdminPanel.php?id=$encryptedUsername'</script>";
+                }
+                elseif($type == "Regular") {
+                    $encryptedUsername = openssl_encrypt($username,'RC4-40','regularUserPassword');
+                    $encryptedUsername = str_replace("+", "!!!", $encryptedUsername);
+                    $encryptedUsername = str_replace("%", "$$$", $encryptedUsername);
+                    $dbcomm->setCurrentDateByUsername($username);
+                    echo "<script>window.location = '../user/Homepage.php?id=$encryptedUsername'</script>";
+                }
             }
-            elseif($type == "User") {
-                $encryptedUsername = Crypto::encrypt($username, true);
-                echo "<script>window.location = '../user/Homepage.php?id=$encryptedUsername'</script>";
+            else {
+                $encryptedUsername = openssl_encrypt("$username", 'RC4', 'sendVerificationEmailPassword');
+                $encryptedUsername = str_replace("+", "!!!", $encryptedUsername);
+                $encryptedUsername = str_replace("%", "$$$", $encryptedUsername);
+                $alert .= "<div class='alert alert-warning alert-dismissible' role='alert'>
+          <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+          <strong>Sorry!</strong> Please verify your account. <a href='../email-sender/AccountVerificationSender.php?id=$encryptedUsername'>Click here</a> to resend the verification email.</div>";
             }
         }
         else {
-            $encryptedUsername = Crypto::encrypt($username, true);
-
-            $alertMessage =
-                '<div class="alert alert-danger alert-dismissible" role="alert">'
-                    .'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
-                        .'<span aria-hidden="true">&times;</span>'
-                    .'</button>'
-                    .'<strong>'
-                        .'Error: '
-                    .'</strong>'
-                    .'<span>'
-                        .'Please verify your account.\n'
-                    .'</span>'
-                    .'<a href="../email-sender/AccountVerificationSender.php?id=$encryptedUsername">'
-                        .'Click here'
-                    .'</a>'
-                    .'<span>'
-                        .' to resend the verification email.'
-                    .'</span>'
-                .'</div>';
+            $alert .= '<div class="alert alert-danger alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <strong>Error!</strong> Incorrect Username/Password</div>';
         }
-    }
-    else {
-        $alertMessage =
-                '<div class="alert alert-danger alert-dismissible" role="alert">'
-                    . '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
-                        .'<span aria-hidden="true">&times;</span>'
-                    .'</button>'
-                    . '<strong>'
-                        . 'Error: '
-                    . '</strong>'
-                    . '<span>'
-                        . 'Incorrect Username/Password'
-                    . '</span>'
-                . '</div>';
     }
 }
 ?>
@@ -161,62 +140,60 @@ if (isset($_POST['submitCredentials'])) {
                             <font color="White" >"To achieve <strong>big</strong> things, start small!"</font>
                         </p>
                     </div>
-                    <?php if (isset($alertMessage)) echo "echo $alertMessage";?>
+
+                    <? if (isset($alert)) //if the alert for creating list is set, then echo the alert
+                    {
+                        echo '<div>';
+                        echo $alert;
+                        echo '</div>';
+                    }
+                    ?>
+
                 </div>
 
-            <div class="col-sm-6 col-sm-offset-3 form-box">
-                <div class="form-top">
-                    <div class="form-top-left">
-                        <h3>Login to our site</h3>
-                        <p>Enter your credentials to log on:</p>
-                    </div>
-                    <div class="form-top-right">
-                        <i class="fa fa-key"></i>
-                    </div>
-                </div>
-                <div class="form-bottom">
-                    <form role="form" action="Login.php" method="post" class="login-form">
-                        <div class="form-group">
-                            <label class="sr-only" for="username">Username</label>
-                            <input type="text" name="username" placeholder="Username..."
-                                   class="form-control" id="username"
-                                   value="<?php echo $username;?>"/>
+                <div class="col-sm-6 col-sm-offset-3 form-box">
+                    <div class="form-top">
+                        <div class="form-top-left">
+                            <h3>Login to our site</h3>
+                            <p>Enter your credentials to log on:</p>
                         </div>
-                        <div class="form-group">
-                            <label class="sr-only" for="password">Password</label>
-                            <input type="password" name="password" placeholder="Password..."
-                                   class="form-control" id="password"
-                                   value="<?php echo $password;?>"/>
+                        <div class="form-top-right">
+                            <i class="fa fa-key"></i>
                         </div>
-                        <table cellpadding="0" cellspacing="0" border="0" style="width:100%;">
-                            <tr>
-                                <td align = "left">
-                                    <div class="form-group">
-                                        <input type="checkbox" name="remember" value="yes" title="Remember Me"/>
-                                    </div>
-                                </td>
-                                <td align = "right">
-                                    <div class = "form-group">
-                                        <a href="../email-sender/RecoveryEmail.php">
-                                            Forgot login?
-                                        </a>
-                                    </div>
-                                </td>
-                            </tr>
-                        </table>
-                        <button class="btn" type="submit" value="postBackCredentials" name="submitCredentials">Login</button>
-                    </form>
-                    <div>
+                    </div>
+                    <div class="form-bottom">
+                        <form role="form" action="Login.php" method="post" class="login-form">
+                            <div class="form-group">
+                                <label class="sr-only" for="signin-username">Username</label>
+                                <input type="text" name="signin-username" placeholder="Username..."
+                                       class="form-control" id="signin-username"
+                                       value="<? echo (isset($cookieUsername))?$cookieUsername:''; ?>">
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only" for="signin-password">Password</label>
+                                <input type="password" name="signin-password" placeholder="Password..."
+                                       class="form-control" id="signin-password"
+                                       value="<? echo (isset($cookiePassword))?$cookiePassword:''; ?>">
+                            </div>
+                            <table cellpadding="0" cellspacing="0" border="0" style="width:100%;">
+                                <tr>
+                                    <td align = "left"><div class="form-group"><input type="checkbox" name="remember" value="yes">&nbsp;&nbsp;Remember me</div></td>
+                                    <td align = "right"><div class = "form-group"><a href="../email-sender/RecoveryEmail.php">Forgot login?</a></div></td>
+                                </tr>
+                            </table>
+                            <button class = "btn" type = "submit" name="Submit">Login</button>
+                        </form>
+                        <div>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class = "row">
-            Don't have an account yet? <a href="CreateAccount.php">Click Here</a>
+            <div class = "row">
+                Don't have an account yet? <a href="CreateAccount.php">Click Here</a>
+            </div>
         </div>
     </div>
-</div>
 
 </div>
 
