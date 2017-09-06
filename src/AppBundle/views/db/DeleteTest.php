6ff0e861f26196ce625626e624faa58b34453a3b
SPARTA-SYSTEMS\andrew.parise <andrew.parise@spartassystems.com>
2017-09-06 15:38:32 -0400

    Andrew P - Commit before project restructure

--- src/AppBundle/views/db/DeleteTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once "dbcomm.php";
-$dbcomm = new dbcomm();
-
-$dbcomm->deleteAccountByUsername("Kev");
---
+++ src/Planbook/AppBundle/Resources/public/js/fullpage-homepage-config.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#7FB3D5', '#7FB3D5', '#7FB3D5'],
+        anchors: ['firstPage', 'secondPage', '3rdPage'],
+        menu: '#menu'
+    });
+});
